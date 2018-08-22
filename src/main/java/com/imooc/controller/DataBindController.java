98480145a0a290837b4c75bd1748909e8770488a
李孟浩 <472794030@qq.com>
2018-08-22 23:41:20 +0800

    环境初始化完成
    如题

---
+++ src/main/java/com/imooc/controller/DataBindController.java
@@ -0,0 +1,12 @@
+package com.imooc.controller;
+
+import com.imooc.dao.CourseDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataBindController {
+
+    @Autowired
+    private CourseDao courseDao;
+
+    
+}
---
+++ src/main/java/com/imooc/entity/Course.java
@@ -0,0 +1,47 @@
+package com.imooc.entity;
+
+/**
+ * 课程类
+ */
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    private Author author;
+
+
+}
