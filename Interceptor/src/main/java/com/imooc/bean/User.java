0d8d86a4830da432abdc67d22d0ec5fb0bae07c5
李孟浩 <472794030@qq.com>
2018-08-24 21:18:47 +0800

    interceptor结构
    如题

---
+++ Interceptor/src/main/java/com/imooc/bean/User.java
@@ -0,0 +1,22 @@
+package com.imooc.bean;
+
+public class User {
+    private String account;//用户名
+    private String password;//密码
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
